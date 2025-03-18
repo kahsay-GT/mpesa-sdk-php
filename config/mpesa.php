a48903a87320c29a4d3ce966caa944d1ef7cbbd2
kahsay21a <kahsay7g@gmail.com>
2025-03-18 21:38:37 +0300

    config updated

--- config/mpesa.php
+++ config/mpesa.php
@@ -5,12 +5,13 @@ return [
         'base_url' => env('DEV_MPESA_BASE_URL', 'YOUR_DEV_MPESA_BASE_URL'),
         'consumer_key' => env('DEV_MPESA_CONSUMER_KEY', 'YOUR_DEV_MPESA_CONSUMER_KEY'),
         'consumer_secret' => env('DEV_MPESA_CONSUMER_SECRET', 'YOUR_DEV_MPESA_CONSUMER_SECRET'),
+        'security_credential' => env('DEV_SECURITY_CREDENTIAL', 'YOUR_DEV_SECURITY_CREDENTIAL'),
     ],
-    'dev_security_credential' => env('DEV_SECURITY_CREDENTIAL', 'YOUR_DEV_SECURITY_CREDENTIAL'),
+    
     'production' => [
         'base_url' => env('PROD_MPESA_BASE_URL', 'YOUR_PROD_MPESA_BASE_URL'),
         'consumer_key' => env('PROD_MPESA_CONSUMER_KEY', 'YOUR_PROD_MPESA_CONSUMER_KEY'),
         'consumer_secret' => env('PROD_MPESA_CONSUMER_SECRET', 'YOUR_PROD_MPESA_CONSUMER_SECRET'),
+        'security_credential' => env('PROD_SECURITY_CREDENTIAL', 'YOUR_PROD_SECURITY_CREDENTIAL'),
     ],
-    'prod_security_credential' => env('PROD_SECURITY_CREDENTIAL', 'YOUR_PROD_SECURITY_CREDENTIAL'),
 ];
